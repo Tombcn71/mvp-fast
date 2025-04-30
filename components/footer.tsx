11d2b23d0fbcd3d1cf1b39188f847e2797927ea9
Tom <tbvanreijn@gmail.com>
2025-04-30 08:56:34 +0200

    t

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
     <footer className="w-full border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          © {new Date().getFullYear()} MVP FAST IS PART OF TOMATINA IDEAS WEB
-          S.L. All rights reserved.
+          © {new Date().getFullYear()} MVP FAST IS PART OF T&T IDEAS WEB S.L.
+          All rights reserved.
         </p>
       </div>
     </footer>
